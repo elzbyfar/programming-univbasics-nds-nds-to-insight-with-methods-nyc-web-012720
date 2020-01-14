71db1a4de40648e42cbbb5d25ebefa3ef6758a37
Luis Alejo <lou.m.alejo@gmail.com>
2020-01-14 01:27:56 +0000

    Automatically backed up by Learn

--- lib/nds_extract.rb
+++ lib/nds_extract.rb
@@ -41,7 +41,7 @@ def directors_totals(nds)
   result = {}
   i = 0
   while i < nds.length do 
-    result[nds[i][:name]] = gross_for_director(nds)[i][:movies]
+    result[nds[i][:name]] = gross_for_director(nds)[i][:name]
     i += 1
   end
  result
